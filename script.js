c148a6cd6b6a57cefcb33a32ab21c6d23b462ae8
Ali Kooshesh <alikooshesh007@gmail.com>
2023-12-25 17:21:26 +0330

    feat/update-todo

--- script.js
+++ script.js
@@ -18,6 +18,23 @@ function deleteTodo(todoId){
     render()
 }
 
+function updateTodo(todoId , newText){
+    let tempTodoList = []
+
+    todoList.forEach(item =>{
+        if(item.id !== todoId){
+            tempTodoList.push(item)
+        }else{
+            item.text = newText
+            tempTodoList.push(item)
+        }
+        
+    })
+
+    todoList = tempTodoList
+    render()
+}
+
 //// Dom
 
 function addBtnClickHandler(){
@@ -27,6 +44,11 @@ function addBtnClickHandler(){
 
 function deleteBtnClickHandler(todoId){
     deleteTodo(todoId)
+}
+
+function editBtnClickHandler(todoId){
+    let newTodoText = prompt(`edit todo id : ${todoId}`)
+    updateTodo(todoId , newTodoText)
 }
 
 function render(){
@@ -44,7 +66,7 @@ function render(){
                           </svg>
                     </button>
     
-                    <button>
+                    <button onclick="editBtnClickHandler(${item.id})">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
